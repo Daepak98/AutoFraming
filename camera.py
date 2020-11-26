75c090602080f7ab8a8b9c4045f3003a23b533b9
daepak98 <bunch.of.food@gmail.com>
2020-11-26 18:50:38 -0500

    Updated Code - Can now track a face using training images from Kaggle set

--- camera.py
+++ camera.py
@@ -3,29 +3,69 @@ from math import ceil
 import cv2
 import numpy as np
 from numpy.lib.function_base import diff
+import random
+import os
 
 deebug = True
-mirrored = False
+mirrored = True
 
 
 class VideoCamera(object):
     def __init__(self):
+        ims = self.__get_training()
+        self.mean_face, self.std_face = self.__computeFaceStats(*ims)
         self.video = cv2.VideoCapture(0)
-        self.training_frames = []  # These vars will be for when
-        self.trained = False      # we have to create our own
-        self.num_train = 60       # classifier/detector
 
     def __del__(self):
         self.video.release()
 
+    def __get_training(self):
+        ims = []
+        imspath = "./training/"
+        dirs = os.listdir(imspath)
+        random.shuffle(dirs)
+        num_keep = 1000
+        for i, name in enumerate(dirs):
+            if num_keep != 0:
+                if i >= num_keep:
+                    break
+            impath = f"{imspath}{name}"
+            im = cv2.imread(impath)
+            im = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
+            ims.append(im[:, :, 1])
+        return ims
+
+    def __computeFaceStats(self, *ims):
+        ims = np.array(ims)
+        mean = np.mean(ims, axis=0)
+        std = np.std(ims, axis=0)
+        return mean, np.sum(std)
+
     def __find_face(self, img):
-        pass
+        mean = self.mean_face
+        std = self.std_face
+        stats = -1*np.ones(img.shape)
+        stats = stats[:-mean.shape[0], :-mean.shape[1]]
+        detail = 12
+        for y in range(0, img.shape[0]-mean.shape[0], int(mean.shape[0]/detail)):
+            for x in range(0, img.shape[1]-mean.shape[1], int(mean.shape[1]/detail)):
+                sub = img[y:y+mean.shape[0], x:x+mean.shape[1]]
+                stats[y, x] = ((sub - mean)/(std)).sum()
+        # stats[stats == 0] = np.max(stats)
+        try:
+            min_y, min_x = np.where(stats == np.max(stats[stats > 0]))
+            min_y = min_y[0]
+            min_x = min_x[0]
+        except:
+            min_y = int(img.shape[0]/2-mean.shape[0]/2)
+            min_x = int(img.shape[1]/2-mean.shape[1]/2)
+        return min_y, min_x, min_y+mean.shape[0], min_x+mean.shape[1]
 
     def __find_face_center(self, img):
         center = (0, 0)
 
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        x, y, w, h = self.__find_face(self, img)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        y, x, h, w = self.__find_face(img_gray[:, :, 1])
 
         x_mean = int((2*x+w)/2)
         y_mean = int((2*y+h)/2)
@@ -64,7 +104,7 @@ class VideoCamera(object):
         return img.copy()
 
     def get_processed(self):
-        ret, frame = self.video.read()
+        _, frame = self.video.read()
 
         # Performance Phase
         center = self.__find_face_center(frame)
@@ -89,7 +129,7 @@ if __name__ == "__main__":
     video_stream = VideoCamera()
     while True:
         frame = video_stream.get_processed()
-        cv2.imshow('my webcam', frame)
+        cv2.imshow("Testing Images", frame)
         if cv2.waitKey(1) == 27:
             break  # esc to quit
     cv2.destroyAllWindows()
--- testing_eig_face.py
+++ testing_eig_face.py
@@ -3,7 +3,10 @@ import cv2
 import numpy as np
 import os
 import pickle as pkl
+import matplotlib.pyplot as plt
+import random
 
+from numpy.core.fromnumeric import mean
 # %% Moving photos from nested folder to `training`
 # import os
 # from os import path
@@ -21,41 +24,118 @@ import pickle as pkl
 #         os.rmdir(imdir)
 
 # %% Train a model
-def computeMeanFace():
-    ims = []
-    mean = []
-    orig_shape = 0
-    imspath = "./training/"
-    dirs = os.listdir(imspath)
-    num_ims = 10
-    dirs = dirs[:num_ims]
-    for i in dirs:
-        impath = f"{imspath}{i}"
-        im = cv2.imread(impath, cv2.IMREAD_GRAYSCALE)
-        orig_shape = im.shape
-        im = im.flatten()
-        if len(ims) == 0:
-            ims = im
-        else:
-            ims = np.vstack((ims, im))
-        # print(impath)
+
+
+def computeFaceStats(*ims):
+    ims = np.array(ims)
     mean = np.mean(ims, axis=0)
-    return ims, mean, orig_shape
+    std = np.std(ims, axis=0)
+    return mean, np.sum(std)
+
 
 def computeFaceCov(ims, mean):
     mean_shifted = np.transpose(ims - mean)
-    np.cov(mean_shifted)
-    pass
+    cov = np.transpose(mean_shifted)@mean_shifted
+    return cov
+
+
+# def getTopMEigVectors(cov):
+#     u, v = np.linalg.eig(cov)
+#     retain_num = 0
+#     max_indices = np.argsort(u)[::-1]
+#     cutoff = 0.9
+#     total = np.sum(u)
+#     while retain_num < len(max_indices):
+#         subset = u[max_indices][:retain_num]
+#         if np.sum(subset)/total >= cutoff:
+#             return subset, v[:, max_indices[:retain_num]]
+#         else:
+#             retain_num += 1
+# %%
+# mean_face = computeFaceStats()
+# # face_cov = computeFaceCov(ims, mean_face)
+# # topVectors = getTopMEigVectors(face_cov)
+
+# # %%
+plt.imshow(mean_face, cmap=plt.cm.gray)
+
+# %% Run a Video Stream
+
+
+# def testMean(img):
+#     cy, cx = tuple(i/2 for i in img.shape)
+#     p1 = tuple(int(i) for i in (cx-125, cy-125))
+#     p2 = tuple(int(i) for i in (cx+125, cy+125))
+#     flat = img[p1[1]:p2[1], p1[0]:p2[0]].flatten()
+#     # img_cov = computeFaceCov(np.array(flat), mean_face)
+#     img[:250, :250] = mean_face.reshape(orig_shape) # cv2.resize(mean_face, tuple(int(i*1) for i in mean_face.reshape(orig_shape).shape[::-1]))
+#     frame = cv2.rectangle(img, p1, p2, (255, 0, 0), 2)
+#     diff = np.sum(np.abs(flat - mean_face))
+#     frame = cv2.putText(frame,
+#                         f"{diff}",
+#                         (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+#     return frame, diff
+
+
+# video_stream = cv2.VideoCapture(0)
+# diffs = []
+# while True and len(diffs) < 600:
+#     ret, frame = video_stream.read()
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     frame, diff = testMean(frame)
+#     diffs.append(int(diff))
+#     cv2.imshow(f"{frame.shape}", frame)
+#     if cv2.waitKey(1) == 27:
+#         break  # esc to quit
+# cv2.destroyAllWindows()
+# diffs = np.array(diffs)
+
+ims = []
+mean_face = []
+imspath = "./training/"
+dirs = os.listdir(imspath)
+# random.shuffle(dirs)
+num_keep = 1000
+for i, name in enumerate(dirs):
+    if num_keep != 0:
+        if i >= num_keep:
+            break
+    impath = f"{imspath}{name}"
+    im = cv2.imread(impath)
+    im = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
+    ims.append(im[:, :, 1])
+mean_face, std_face = computeFaceStats(*ims)
 
 # %%
 
-ims, mean_face, orig_shape = computeMeanFace()
-face_cov = computeFaceCov(ims, mean_face)
-# %% Run a Video Stream
+
+def test(img, mean, std):
+    stats = -1*np.ones(img.shape)
+    stats = stats[:-mean.shape[0], :-mean.shape[1]]
+    for y in range(0, img.shape[0]-mean.shape[0], int(mean.shape[0]/4)):
+        for x in range(0, img.shape[1]-mean.shape[1], int(mean.shape[1]/4)):
+            sub = img[y:y+mean.shape[0], x:x+mean.shape[1]]
+            stats[y, x] = ((sub - mean)/(std)).sum()
+    # stats[stats == 0] = np.max(stats)
+    min_y, min_x = np.where(stats == np.max(stats[stats > 0]))
+    min_y = min_y[0]
+    min_x = min_x[0]
+    return min_y, min_x, min_y+mean.shape[0], min_x+mean.shape[1]
+
+
 video_stream = cv2.VideoCapture(0)
 while True:
     ret, frame = video_stream.read()
-    cv2.imshow('my webcam', frame)
+    frame = cv2.flip(frame, 1)
+    process_this = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+    min_y, min_x, min_h, min_w = test(
+        process_this[:, :, 1], mean_face, std_face)
+    frame[min_y:min_h, min_x:min_w, 1] = mean_face
+    frame = cv2.rectangle(frame, (min_x, min_y),
+                          (min_w, min_h), (0, 0, 0), 2)
+    # cv2.imshow("HSV", process_this[:,:,1])
+    cv2.imshow("Testing", frame)
     if cv2.waitKey(1) == 27:
         break  # esc to quit
 cv2.destroyAllWindows()
+video_stream.release()
